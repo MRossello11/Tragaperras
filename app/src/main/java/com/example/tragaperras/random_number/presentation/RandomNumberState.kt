00284f1a8016ff6f5b2356809b4c718cc89da07a
MRossello11 <miquel.rossello.mas@gmail.com>
2025-05-23 09:20:52 +0200

    Changed: Renaming

--- app/src/main/java/com/example/tragaperras/random_number/presentation/RandomNumberState.kt
+++ app/src/main/java/com/example/tragaperras/random_number/presentation/RandomNumberState.kt
@@ -2,9 +2,9 @@ package com.example.tragaperras.random_number.presentation
 
 data class RandomNumberState(
     val randomNumber: Int? = null,
-    val selectedNumber: Int = 0,
+    val guess: Int = 0,
     val credit: Int = 0,
     val showNumber: Boolean = false,
     val textToShow: String? = null,
-    val randomNumberTextEvent: RandomNumberTextEvent? = null
+    val guessOutcomeEvent: GuessOutcomeEvent? = null
 )
