b6b8ac39ea7d83be275e2562651c60eacfc0c947
MRossello11 <miquel.rossello.mas@gmail.com>
2025-07-02 14:57:12 +0200

    Added: di to feature_credit

--- feature_credit/build.gradle.kts
+++ feature_credit/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    id("org.jetbrains.kotlin.kapt")
+    id("dagger.hilt.android.plugin")
 }
 
 android {
@@ -35,11 +37,27 @@ android {
 dependencies {
 
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.lifecycle.runtime.compose.android)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+
+    //Dagger - Hilt
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.android.compiler)
+    kapt(libs.androidx.hilt.compiler)
+    implementation(libs.androidx.hilt.navigation.compose)
 
     implementation(project(":database"))
 }
---
+++ feature_credit/src/main/java/com/example/feature_credit/di/CreditModule.kt
@@ -0,0 +1,34 @@
+package com.example.feature_credit.di
+
+import com.example.feature_credit.data.CreditRepositoryImpl
+import com.example.feature_credit.domain.UpdateCreditUseCase
+import com.example.feature_credit.domain.repository.CreditRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import slot.machine.database.model.CreditDao
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object CreditModule {
+
+    @Provides
+    @Singleton
+    fun provideCreditRepository(
+        creditDao: CreditDao
+    ) : CreditRepository {
+        return CreditRepositoryImpl(creditDao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideUpdateCreditUseCase(
+        creditRepository: CreditRepository
+    ): UpdateCreditUseCase {
+        return UpdateCreditUseCase(
+            creditRepository
+        )
+    }
+}
