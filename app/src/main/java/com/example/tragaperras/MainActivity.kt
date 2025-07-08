958ef5e6f6e9e371d289a79b3e6e578e6789f5eb
MRossello11 <miquel.rossello.mas@gmail.com>
2025-07-08 15:58:37 +0200

    Added: credit feature to app

--- app/src/main/java/com/example/tragaperras/MainActivity.kt
+++ app/src/main/java/com/example/tragaperras/MainActivity.kt
@@ -5,16 +5,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
-import com.example.feature_random_number.random_number.presentation.RandomNumberScreen
-import com.example.feature_random_number.random_number.presentation.RandomNumberViewModel
-import com.example.core.presentation.Topbar
-import com.example.core.presentation.TopbarState
+import com.example.tragaperras.core.presentation.navigation.Navigation
 import com.example.tragaperras.ui.theme.TragaperrasTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,12 +17,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TragaperrasTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { _ ->
-                    Column {
-                        Topbar(TopbarState("Random number"))
-                        RandomNumberScreen(RandomNumberViewModel())
-                    }
-                }
+                Navigation()
             }
         }
     }
---
+++ app/src/main/java/com/example/tragaperras/core/commons/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.tragaperras.core.commons
+
+object Constants {
+    object Routes {
+        const val RANDOM_NUMBER_ROUTE = "RANDOM_NUMBER_ROUTE"
+        const val CREDIT_ROUTE = "CREDIT_ROUTE"
+    }
+}
